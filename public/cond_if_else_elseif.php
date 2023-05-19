fbc59895d4ef8ac68f689240abacaa47de18bb2a
Lipszera <nothefallen@gmail.com>
2023-05-18 21:00:22 -0300

    condicionais if, else e elseif

--- public/cond_if_else_elseif.php
+++ public/cond_if_else_elseif.php
@@ -47,3 +47,22 @@ if ($a/$b==2) {
 } else {
     echo "O resultado da divisão não é 2 nem 4";
 }
+
+
+
+$tornado250 = "HONDA";
+$lander250 = "YAMAHA";
+$gs1200 = "BMW";
+
+$moto = readline("Qual moto você teria: HONDA, YAMAHA OU BMW?\n");
+
+if ($moto == "HONDA") {
+    echo "MOTO DE HOMEM, HONDA É HONDA";
+} elseif ($moto == "YAMAHA") {
+    echo "YAMAHA É MOTO DE MENININHA";
+} elseif ($moto == "BMW") {
+    echo "BMW É TOP, AI O PAU PEGA";
+} else {
+    echo "OPÇÃO INVÁLIDA";
+}
+
